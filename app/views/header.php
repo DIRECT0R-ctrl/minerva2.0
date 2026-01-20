91a4790d70aaeffaebcdc49030026879a02f4a72
saraabbih1 <abbihsara30@gmail.com>
2026-01-20 09:55:47 +0100

    feat:creation de  footer et header

---
+++ app/views/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Minerva</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+<header>
+    <h1> Minerva Platform</h1>
+</header>
