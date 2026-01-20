f7df1fb957efd4ba70a458ccc394a9622940e0fb
DIRECT0R-ctrl <DominantVortex@proton.me>
2026-01-20 14:19:43 +0100

    making the database tables with postgres + config db => object

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php 
+declare(strict_types=1);
+
+return [
+	'db' => [
+		'driver' => 'pgsql',
+		'host' => '127.0.0.1',
+		'port' => 5432,
+		'dbname'=> 'minerva',
+		'user' => 'postgres',
+		'password' => 'typhussama5T!',
+	],
+			
+];
+?>
