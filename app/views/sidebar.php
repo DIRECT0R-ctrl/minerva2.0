e60250d2e92209d8bd54248f0ce8a46efe0884a8
saraabbih1 <abbihsara30@gmail.com>
2026-01-20 12:21:29 +0100

    feat:sidebar.ph

---
+++ app/views/sidebar.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="dashboard_student.php">Dashboard</a></li>
+        <li><a href="mes_travaux.php">Mes travaux</a></li>
+        <li><a href="notes.php">Mes notes</a></li>
+        <li><a href="chat_class.php">Chat de classe</a></li>
+        <li><a href="logout.php">Déconnexion</a></li>
+    </ul>
+</nav>
