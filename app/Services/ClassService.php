16aaf7f6fb33104fb1b4b0182d681d36ea860321
saraabbih1 <abbihsara30@gmail.com>
2026-01-21 12:05:33 +0100

    feat: gestion classes (controller, service, view)

---
+++ app/Services/ClassService.php
@@ -0,0 +1,27 @@
+<?php
+require_once __DIR__ . '/../core/Database.php';
+
+class ClassService
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+    public function createClass($name, $teacherId)
+    {
+        $sql = "INSERT INTO classes (name, teacher_id) VALUES (?, ?)";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$name, $teacherId]);
+    }
+
+    public function getClassesByTeacher($teacherId)
+    {
+        $sql = "SELECT * FROM classes WHERE teacher_id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$teacherId]);
+       return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
