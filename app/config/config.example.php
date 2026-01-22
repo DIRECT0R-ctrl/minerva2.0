4ed4d43fa4abd83cff196ece5f302009b4a61339
saraabbih1 <abbihsara30@gmail.com>
2026-01-22 10:19:17 +0100

    Add gitignore and config template

---
+++ app/config/config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'db' => [
+    'driver' => 'CHANGE_ME',   // mysql OR pgsql
+    'host' => '127.0.0.1',
+    'port' => 0,               // 3306 for mysql / 5432 for pgsql
+    'dbname'=> 'minerva',
+    'user' => 'root',          // mysql usually root
+    'password' => 'CHANGE_ME',
+  ],
+];
